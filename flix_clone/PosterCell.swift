c5402685b47beb9b09c39f6ff7bbf1e7ef1b0d0e
justin.lee@corebaby.org <TURNIPZ@Justins-MacBook-Pro-3.local>
2018-02-12 20:23:56 -0800

    Got the assignment 2 finished and add in gif

---
+++ flix_clone/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_clone
+//
+//  Created by Justin Lee on 2/6/18.
+//  Copyright © 2018 Justin Lee. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
